3363ff5ce7af1396ef07004095a708c7a44453b8
Md Imran Hossain <imranhossain1402@gmail.com>
2023-10-16 16:15:01 +0200

    Final update

--- dist/app/modules/bus/bus.constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.busSearchableFields = exports.busFilterableFields = void 0;
-exports.busFilterableFields = ['searchTerm', 'busNumber', 'busType'];
-exports.busSearchableFields = ['busNumber'];
